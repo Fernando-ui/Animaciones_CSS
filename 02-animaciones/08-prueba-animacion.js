11faa5ec4ba37c2a0c40933ae998551e35ff67d7
Fernando <fst_96@hotmail.com>
2021-12-29 17:23:45 -0600

    Terminando shorthand animation

---
+++ 02-animaciones/08-prueba-animacion.js
@@ -0,0 +1,6 @@
+const pelota1 = document.querySelector('.pelota');
+pelota1.addEventListener('click',()=>{
+    console.log('Haciendo click en la pelota');
+    pelota1.classList.toggle('animacionFer')
+    
+})
