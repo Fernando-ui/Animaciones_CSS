aae9be8979fc3f72ab3a379e46612035076d2cc8
Fersan <fst-96@hotmail.com>
2021-12-30 21:00:15 -0600

    Detalles pequenios

--- 02-animaciones/08-prueba-animacion.js
+++ 02-animaciones/08-prueba-animacion.js
@@ -1,6 +1,5 @@
 const pelota1 = document.querySelector('.pelota');
 pelota1.addEventListener('click',()=>{
-    console.log('Haciendo click en la pelota');
     pelota1.classList.toggle('animacionFer')
     
 })
