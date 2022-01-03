724ab654c0378e67ee8c97632b89308b218a83e4
Fernando <fst_96@hotmail.com>
2022-01-03 13:20:00 -0600

    Anadiendo animacion de scroll

--- 03-animacionesll/02-animaciones-scroll.js
+++ 03-animacionesll/02-animaciones-scroll.js
@@ -1,8 +1,14 @@
-let images = Array.from(document.querySelectorAll('img'));
-
-let imagesScrollTop = images.map( image => 
-    image.getBoundingClientRect().top
-    
-)
+let images = Array.from(document.querySelectorAll("img"));
+let imagesScrollTop = images.map((image) => image.getBoundingClientRect().top - (innerHeight / 2));
 
 console.log(imagesScrollTop);
+
+window.addEventListener("scroll", () => {
+  imagesScrollTop.forEach((el, i) => {
+    if (scrollY >= el) {
+      images[i].classList.add("animate");
+    } 
+  });
+});
+
+
